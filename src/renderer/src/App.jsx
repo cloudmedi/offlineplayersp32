08eaf53c60ff0d11eba0a8a3cc72dab9987b226f
unknown <coruk_61@hotmail.com>
2024-08-08 19:17:12 +0300

    yeni tasarım

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Home from './components/home/home';
 import Playlist from './components/playist/playlist';
-import axios from 'axios';
 
 function App() {
   const [user, setUser] = useState(null);
-  const [userInfo, setUserInfo] = useState(null);
   const [isLoggin, setIsLoggin] = useState(false);
   const [updateMessage, setUpdateMessage] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -31,15 +29,8 @@ function App() {
   useEffect(() => {
     if (user) {
       setIsLoggin(true);
-      axios.get(`https://app.cloudmedia.com.tr/api/usermaninfo/${user.user.user.id}`).then(res=>{
-      if(res.data){
-        setUserInfo(res.data)
-      }
-      })
     }
   }, [user]);
-
-console.log(userInfo)
 
   useEffect(() => {
     window.electron.ipcRenderer.send("updateMessage");
@@ -54,28 +45,33 @@ console.log(userInfo)
     });
     return () => {
       window.electron.ipcRenderer.removeAllListeners("update-message-reply");
-     
+
     };
   }, [updateMessage]);
-  
+
 
   const handleUpdate = () => {
     window.electron.ipcRenderer.send('start-update');
     setIsModalOpen(false);
   };
- 
+
   return (
     <>
-      {isLoggin ? <Playlist data={user} user={userInfo}  /> : <Home />}
-       {updateMessage==="Güncelleme mevcut." && (
-        <div className="modal" style={{ display: isModalOpen ? 'block' : 'none' }}>
-          <div className="modal-content">
-            <span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
-            <p>{updateMessage}</p>
-          { updateMessage==="Güncelleme indiriliyor."?null:<button className='btn' onClick={handleUpdate}>Güncellemeyi Başlat</button>}
+      {isLoggin ? <Playlist data={user} /> : <Home />}
+      {updateMessage === "Güncelleme mevcut." && (
+        <div className="modal1" style={{ display: isModalOpen ? 'block' : 'none' }}>
+          <div className="modal-content1">
+            <div style={{ display: "flex", flexDirection: "row",alignItems:"center",justifyContent:"space-between" }}>
+              <span style={{
+                fontFamily: "Arial", fontSize: "20px", fontWeight: "700", lineHeight: "23px", textAlign: "left",color:"black"
+              }}>{updateMessage}</span>
+              <span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
+            </div>
+            <p>Müzik uygulamamız için yeni bir güncelleme mevcut. En son özellikleri kullanmak için lütfen uygulamayı güncelleyin.</p>
+            {updateMessage === "Güncelleme indiriliyor." ? null : <button className='btn-mes' onClick={handleUpdate}>Uygulamayı Güncelle</button>}
           </div>
         </div>
-      )} 
+      )}
     </>
   );
 }
--- src/renderer/src/components/playist/playlist.jsx
+++ src/renderer/src/components/playist/playlist.jsx
@@ -5,111 +5,86 @@ import Player from "../player/player"
 import Countdown from "react-countdown";
 import axios from "axios";
 import _ from "lodash";
+import logout from "./Log_Out.png"
 function Playlist(props) {
 
 
     const [selectedPlaylist, setSelectedPlaylist] = useState([]);
-    const [click,setClick]=useState(0)
-
-    const [time, setTime] = useState(new Date());
-
-    useEffect(() => {
-      const timer = setInterval(() => {
-        setTime(new Date());
-      }, 1000);
-  
-      return () => {
-        clearInterval(timer);
-      };
-    }, []);
-    const user=props?.data?.user?.user
-   
-    const hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-
+    const [click, setClick] = useState(0)
+    const user = props?.data?.user?.user
+    const [showLogout,setShowLogOut]=useState(false) 
     const handlePlaylistClick = (playlist) => {
         setSelectedPlaylist(playlist);
         setClick(prevClick => prevClick + 1);
         // Burada seçilen playlisti savedPlaylists array'ine ekleyebilirsiniz.
-        
+
     };
 
     var tarih = new Date(props.data.user.expires_at);
-   
+
     var zamanDamgasi = tarih.getTime();
     if (Date.now() + (Number(zamanDamgasi) - Date.now()) === 0) {
         logOut()
     }
     function logOut() {
-        window.electron.ipcRenderer.send("log-out", "log-out");
-        window.location.reload()
-    }
-   
-    
-
-    
-    useEffect(() => {
+        setShowLogOut(true)
       
-            // Kullanıcı yöneticiyse ve manager_id 951 ise
-            if (props?.user?.userinfo?.manager_id !== null && props?.user?.userinfo?.manager_id === 951) {
+    }
 
-                const playlists = props.data.allPlaylists; // Tüm çalma listeleri
-    
-                const selectedPlaylist = playlists.map(playlist => {
-                    // Saat 8 ile 11 arası ve "Hareketli" çalma listesi ise
-                    if (hours >= 8 && hours < 11 && playlist.playlistName === "Slow") {
-                        setSelectedPlaylist(playlist)
-                    }
-                    // Saat 11 ile 00:00 arası ve "Slow" çalma listesi ise
-                    else if ((hours >= 11 ) && playlist.playlistName === "Hareketli") {
-                        setSelectedPlaylist(playlist)
-                    }
-                    // Diğer durumlarda null döndür
-                    return null;
-                }).filter(playlist => playlist !== null); // null olmayanları filtrele
-    
-              
-            }
-       // Her dakikada bir kontrol etmek için 60000 milisaniye (1 dakika)
-    
-       
-    }, [props?.user?.userinfo,hours]); // props.data.allPlaylists bağımlılığını ekledik
-    
-    
+function logOut2(){
+      window.electron.ipcRenderer.send("log-out", "log-out");
+        window.location.reload() 
+}
+
+
+
+
+
+
 
 
 
     return (
         <>
             <div style={{ width: window.innerWidth, marginBottom: "150px" }} className="">
-                <nav class="navbar">
-                    <div class="navbar-content">
-                        <div class="navbar-logo">
-                            <img src={props?.data.user?.user?.artwork_url} alt="Logo" />
-                            <span>{props?.data?.user?.user?.name}</span>
-                        </div>
-                        {/* <div style={{ display: "flex", flexDirection: "column" }} className="navbar-info">
-                            <span>Lisans Bitiş Süresi</span>
-                            <span><Countdown date={Date.now() + (Number(zamanDamgasi) - Date.now())} /></span>
-                        </div> */}
-                        <div class="navbar-links">
-                            <a href="#" onClick={() => logOut()}>Çıkış Yap</a>
-                        </div>
+                <div className="frame">
+                    <div className="div-wrapper">
+                        <div className="text-wrapper">{user?.name}</div>
                     </div>
-                </nav>
+                  
+                  {/*   <div className="div">
+                        <input className="r-n-kategori-veya" placeholder="Ara"/>
+                        <Magnifer className="outline-search" /> 
+                    </div> */}
+                      
+                      <div className="logout">
+                      <img style={{cursor:"pointer"}}  alt="Interface log out" src={logout}  onClick={()=>logOut()}/>
+                      </div>
+                </div>
                 <div class="playlist-container">
                     {props?.data?.allPlaylists?.map(res => (
                         <div className="playlist" key={res?.playlistName} style={{ cursor: "pointer" }} onClick={() => handlePlaylistClick(res)}>
                             <img src={res.playlistImage} alt={res.playlistName} />
-                            <h3 >{res.playlistName}</h3>
+                            <span >{res.playlistName}</span>
                         </div>
                     ))}
                 </div>
             </div>
             <div style={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 99999 }}>
-                <Player data={{selectedPlaylist,user,click}}/>  
+                <Player data={{ selectedPlaylist, user, click }} />
             </div>
+            {showLogout === true && (
+        <div className="modal2" style={{ display:  'block'  }}>
+          <div className="modal-content2">
+           
+            <p>Çıkış yapmak istiyor musun?</p>
+            <div style={{ display: "flex", flexDirection: "row",alignItems:"center",justifyContent:"center",gap:"20px" }}>
+            <button className='btn-mes1' onClick={()=>logOut2()} >Evet</button>
+            <button className='btn-mes1' onClick={() => setShowLogOut(false)} >Hayır</button>
+            </div>
+          </div>
+        </div>
+      )}
         </>
     );
 
